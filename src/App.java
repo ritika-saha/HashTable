645e2e9207ace20d21de928ecd937af91f756aa5
ritika-saha <ritika882001@gmail.com>
2023-11-30 01:48:13 +0530

    initialization

---
+++ src/App.java
@@ -0,0 +1,6 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Welcome to hashTable program to calculate word frequency");
+        
+    }
+}
